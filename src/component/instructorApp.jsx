815eae24c2f4f10d5caa657e2639b4da03366c9d
adi-howdy <adityakhatri123@gmail.com>
2019-09-12 19:19:38 -0700

    basic frontend

---
+++ src/component/instructorApp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class InstructorApp extends Component {
+    state = {  }
+    render() { 
+        return (
+            <h1>Instructor Application</h1>
+          );
+    }
+}
+ 
+export default InstructorApp;
